30b64ade4bca5d4c3fd5cd2d10c34d2c14a4f078
Roman <demkivroman5@gmail.com>
2019-10-09 22:38:31 +0300

    Add tast DummyBot

---
+++ chapter_001/src/main/java/job4j/condition/DummyBot.java
@@ -0,0 +1,13 @@
+package job4j.condition;
+
+public class DummyBot {
+    public static String answer(String question){
+        String rsl = "Єто ставит меня в тупик. Задайте другой вопрос.";
+        if("Привет, Бот.".equals(question)){
+            rsl = "Привет, умник";
+        }else if("Пока.".equals(question)){
+            rsl = "До скорой встречи";
+        }
+        return rsl;
+    }
+}
---
+++ chapter_001/src/main/java/job4j/condition/DummyBotTest.java
@@ -0,0 +1,33 @@
+package job4j.condition;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+import org.junit.jupiter.api.Test;
+
+
+public class DummyBotTest
+{
+    @Test
+    public void whenGreetBot(){
+        assertThat(
+             DummyBot.answer("Привет, Бот."),
+                is("Привет, умник")
+        );
+
+    }
+    @Test
+    public void whenByeBot(){
+        assertThat(
+                DummyBot.answer("Пока."),
+                is("До скорой встречи")
+        );
+    }
+    @Test
+    public void whenUnknownBot(){
+        assertThat(
+                DummyBot.answer("Сколько будет 2 + 2?"),
+                is("Єто ставит меня в тупик. Задайте другой вопрос.")
+        );
+    }
+
+}

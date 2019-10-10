c142c9f772f46e332aeb195c36df762e6963a45f
Roman <demkivroman5@gmail.com>
2019-10-10 23:35:16 +0300

    Add task SQMax refactoring

---
+++ chapter_001/src/test/java/ru.job4j.condition/SqMaxTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.condition;
+
+import job4j.condition.SqMax;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SqMaxTest {
+    @Test
+    public void maxFirst(){
+        int result = SqMax.max(100,5,10,2);
+        assertThat(result, is(100));
+    }
+    @Test
+    public void maxSecond(){
+        int result = SqMax.max(1,50,10,2);
+        assertThat(result, is(50));
+    }
+    @Test
+    public void maxThird(){
+        int result = SqMax.max(1,5,10,2);
+        assertThat(result, is(10));
+    }
+    @Test
+    public void maxForth(){
+        int result = SqMax.max(100,5,10,200);
+        assertThat(result, is(200));
+    }
+    @Test
+    public void allEqual(){
+        int result = SqMax.max(100,100,100,100);
+        assertThat(result, is(100));
+    }
+}

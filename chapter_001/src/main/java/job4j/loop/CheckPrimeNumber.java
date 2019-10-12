2e24226a0decda4efd864c86a9c27d70ce5a330d
Roman <demkivroman5@gmail.com>
2019-10-12 18:44:40 +0300

    Add task CheckPrimeNumber

---
+++ chapter_001/src/main/java/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,14 @@
+package job4j.loop;
+
+public class CheckPrimeNumber {
+    public boolean check(int num){
+        int count = 0;
+        for (int i = 1; i <= num; i++){
+            if ((num % i) == 0){
+                count++;
+            }
+        }
+        boolean prime = count > 2 ? false : true;
+        return prime;
+    }
+}

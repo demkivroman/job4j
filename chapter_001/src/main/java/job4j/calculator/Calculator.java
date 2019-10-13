cb7ff7d9848d09044036786621bc2eb12f708488
Roman <demkivroman5@gmail.com>
2019-10-13 22:28:46 +0300

    Add JavaDog file

--- chapter_001/src/main/java/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/job4j/calculator/Calculator.java
@@ -1,23 +1,56 @@
-package ru.job4j.calculator;
+package job4j.calculator;
 
+/**
+ * Class for making aryphmetic operations
+ * @author demkiv
+ * @since 12.10.2019
+ * @version 1
+ */
 public class Calculator {
-
+    /**
+     * @param first first parameter
+     * @param second second parameter
+     * @return result of adding operation
+     */
     public static void add(double first, double second){
         double result = first + second;
         System.out.println(first + "+" + second + "=" + result);
     }
+
+    /**
+     * @param first
+     * @param second
+     * @return result of dividing operation
+     */
     public static void div(double first, double second){
         double result = first / second;
         System.out.println(first + "/" + second + "=" + result);
     }
+
+    /**
+     * @param first
+     * @param second
+     * @return result of multiplying operation
+     */
     public static void multiply(double first, double second){
         double result = first * second;
         System.out.println(first + "*" + second + "=" + result);
     }
+
+    /**
+     * @param first
+     * @param second
+     * @return result of substrackt operation
+     */
     public static void subtrack(double first, double second){
         double result = first - second;
         System.out.println(first + "-" + second + "=" + result);
     }
+
+    /**
+     * Method main accomplish mathemetic operation
+     * @param args
+     */
     public static void main(String[] args){
         add(1,1);
         div(4,2);
---
+++ chapter_001/src/main/java/job4j/calculator/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Roman Demkiv (romandemkiv5@gmail.com)
+ * @version $1$
+ * @since 2019
+ */

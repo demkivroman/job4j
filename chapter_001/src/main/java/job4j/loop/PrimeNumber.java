e1168e556d7633b3ac667fd97793a6e8fa2b3cb0
Roman <demkivroman5@gmail.com>
2019-10-13 15:03:49 +0300

    Correction to PrimeNumber

--- chapter_001/src/main/java/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/job4j/loop/PrimeNumber.java
@@ -3,16 +3,11 @@ package job4j.loop;
 public class PrimeNumber {
     public int calc(int finish){
         int count = 0;
-        boolean prime = false;
+        CheckPrimeNumber primeNumber = new CheckPrimeNumber();
         for (int i=2; i <= finish; i++){
-            int countCoin = 0;
-            for (int j = 1; j <= i; j++){
-                if (i % j == 0){
-                    countCoin ++;
-                }
-            }
-            if(countCoin <= 2)
+            if(primeNumber.check(i)){
                 count++;
+            }
         }
         return count;
     }

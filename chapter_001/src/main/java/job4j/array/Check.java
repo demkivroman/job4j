bb5445ec010947aadaf4095c05b818d93ee62f95
Roman <demkivroman5@gmail.com>
2019-10-16 15:08:49 +0300

    Task [Array - Check]

---
+++ chapter_001/src/main/java/job4j/array/Check.java
@@ -0,0 +1,15 @@
+package job4j.array;
+
+public class Check {
+    public boolean mono(boolean[] data){
+        boolean result = true;
+        boolean tample = data[0];
+        for (int index = 0; index < data.length; index++){
+            if(data[index] != tample){
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}

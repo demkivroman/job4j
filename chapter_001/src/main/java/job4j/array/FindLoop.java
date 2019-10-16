ad4293899f55d1edf27799daea7ea7a714d81b51
Roman <demkivroman5@gmail.com>
2019-10-16 17:57:33 +0300

    Task [ArrayLoop - method indexOf with start & finish]

--- chapter_001/src/main/java/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/job4j/array/FindLoop.java
@@ -11,4 +11,23 @@ public class FindLoop {
         }
         return rsl;
     }
+
+    /**
+     * Method for looking for index of an element in bounds
+     * @param data - array
+     * @param el - found element
+     * @param start - position to start search
+     * @param finish - position to end search
+     * @return - index of found element
+     */
+    public static int indexOf(int[] data, int el, int start, int finish){
+        int rsl = -1;
+        for (int index = start; index < finish; index++){
+            if(data[index] == el){
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
 }

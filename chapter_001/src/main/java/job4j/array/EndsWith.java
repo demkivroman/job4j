2f9b56fb20b906fa05c40a8ac4b5b376fa4b2418
Roman <demkivroman5@gmail.com>
2019-10-16 15:51:22 +0300

    Task [Array - EndsWith]

---
+++ chapter_001/src/main/java/job4j/array/EndsWith.java
@@ -0,0 +1,16 @@
+package job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post){
+        boolean result = true;
+        if(post.length <= word.length){
+            for (int index = 0; index < post.length; index++){
+                if(post[post.length - index - 1] != word[word.length - index - 1]){
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}

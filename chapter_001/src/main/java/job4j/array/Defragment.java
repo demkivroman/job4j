1845504a8904456de5a93895d0ede057f3984a36
Roman <demkivroman5@gmail.com>
2019-10-18 19:53:28 +0300

    Task [Array - Defragment]

---
+++ chapter_001/src/main/java/job4j/array/Defragment.java
@@ -0,0 +1,34 @@
+package job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array){
+        for(int index = 0; index < array.length; index++){
+            String cell = array[index];
+            if (cell == null){
+                // переместить первую не null ячейку
+                // Здесь нужен цикл while
+                int i = index;
+                while (array[i] == null){
+                    i++;
+                    if (i == 7)
+                        break;
+                }
+                if(i < array.length){
+                    array[index] = array[i];
+                    array[i] = null;
+                }
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}

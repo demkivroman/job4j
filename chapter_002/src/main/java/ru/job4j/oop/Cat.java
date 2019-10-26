67c20208f015babff63438267e708cb1a34df7fe
Roman <demkivroman5@gmail.com>
2019-10-26 22:17:51 +0300

    Class extension [OOP - Cat]

--- chapter_002/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,22 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+    public void show() {
+        System.out.println("Cat " + name + " eat " + food);
+    }
+    public void eat(String meat) {
+        this.food = meat;
+    }
     public static void main(String[] args) {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
+        peppy.giveNick("black");
+        peppy.eat("fish");
+        peppy.show();
     }
 }

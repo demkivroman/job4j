7e9ba6c5f9b16a382fcee1edc4ada99546460582
Roman <demkivroman5@gmail.com>
2019-11-10 22:35:30 +0200

    Add tests to StartUITest for replace and delete items

--- chapter_002/src/test/java/tracker/StartUITest.java
+++ chapter_002/src/test/java/tracker/StartUITest.java
@@ -17,4 +17,30 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
     }
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                item.getId(),
+                "replaced item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("delete item");
+        tracker.add(item);
+        String[] answers = {
+                item.getId()
+        };
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item found = tracker.findById(answers[0]);
+        Item expected = null;
+        assertThat(found, is(expected));
+    }
 }

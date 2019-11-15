c3c81596a74151ea30e24430614dfd4abba829b5
Roman <demkivroman5@gmail.com>
2019-11-15 20:13:46 +0200

    Refactor class [PaintTest] add @before and @After anotations

--- chapter_002/src/test/java/strategy/PaintTest.java
+++ chapter_002/src/test/java/strategy/PaintTest.java
@@ -1,8 +1,11 @@
 package strategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import ru.job4j.strategy.Paint;
 import ru.job4j.strategy.Square;
+import ru.job4j.strategy.Triangle;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -10,12 +13,30 @@ import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * @author Roman Demkiv (demkivroman5@gmail.com)
+ * @version $1$
+ * @since 15.11.2019
+ */
 public class PaintTest {
+    // field contain default output to console
+    private final PrintStream stdout = System.out;
+    // buffer for result
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
                 new String(out.toByteArray()),
@@ -31,6 +52,21 @@ public class PaintTest {
                                 .append(System.lineSeparator()).toString()
                 )
         );
-        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        Triangle triangle = new Triangle();
+        assertThat(
+                triangle.draw(),
+                is(new StringBuilder()
+                        .append("      x     ")
+                        .append(System.lineSeparator())
+                        .append("    x x x   ")
+                        .append(System.lineSeparator())
+                        .append("  x x x x x  ")
+                        .append(System.lineSeparator())
+                        .append("x x x x x x x ").toString())
+        );
     }
 }

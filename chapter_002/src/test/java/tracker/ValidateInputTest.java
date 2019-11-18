7f330f6e67c5f93c1c058bd2112dfff29a03a5aa
Roman <demkivroman5@gmail.com>
2019-11-18 19:46:38 +0200

    Refactor [ValidateInputTest] add validation on 'Illegal exception'

--- chapter_002/src/test/java/tracker/ValidateInputTest.java
+++ chapter_002/src/test/java/tracker/ValidateInputTest.java
@@ -1,5 +1,7 @@
 package tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import ru.job4j.tracker.StubInput;
 import ru.job4j.tracker.ValidateInput;
@@ -11,19 +13,40 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class ValidateInputTest {
+    private final PrintStream def = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(def);
+    }
+
     @Test
     public void whenInvalidInput() {
-        ByteArrayOutputStream mem = new ByteArrayOutputStream();
-        PrintStream out = System.out;
-        System.setOut(new PrintStream(mem));
         ValidateInput input = new ValidateInput(
                 new StubInput(new String[] {"invalid", "1"})
         );
         input.askInt("Enter", 1);
         assertThat(
-                new String(mem.toByteArray()),
+                new String(this.out.toByteArray()),
                 is(String.format("Please enter validate data again%n"))
         );
-        System.setOut(out);
+    }
+
+    @Test
+    public void whenIllegalStateException() {
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[] {"90", "2"})
+        );
+        input.askInt("Enter", 5);
+        assertThat(
+                new String(this.out.toByteArray()),
+                is(String.format("Please select key from menu%n"))
+        );
     }
 }

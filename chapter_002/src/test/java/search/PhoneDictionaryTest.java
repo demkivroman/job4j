ef92950131d702b746db986ede9f11cd22ded84d
Roman <demkivroman5@gmail.com>
2019-11-28 22:02:08 +0200

    Collection ArrayList [Person] & [PhoneDictionary]

---
+++ chapter_002/src/test/java/search/PhoneDictionaryTest.java
@@ -0,0 +1,27 @@
+package search;
+
+import org.junit.Test;
+import ru.job4j.search.Person;
+import ru.job4j.search.PhoneDictionary;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PhoneDictionaryTest {
+
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("Petr");
+
+        assertThat(
+                persons.iterator().next().getSurname(),
+                is("Arsentev")
+        );
+    }
+}

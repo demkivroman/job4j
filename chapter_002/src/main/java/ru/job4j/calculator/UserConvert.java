4193baa5e3bf075a1231f96432b508a3d3646301
Roman <demkivroman5@gmail.com>
2019-12-19 10:50:15 +0200

    Refactor add lamda expression to {constructor} of class [UserConvert]

--- chapter_002/src/main/java/ru/job4j/calculator/UserConvert.java
+++ chapter_002/src/main/java/ru/job4j/calculator/UserConvert.java
@@ -13,9 +13,9 @@ public class UserConvert {
         }
         @Override
         public String toString() {
-            return "User{" +
-                    "name='" + name + '\'' +
-                    '}';
+            return "User{"
+                    + "name='" + name + '\''
+                    + '}';
         }
 
     }

75d5ca7aded9817717a503eb54e893d4ab4dc3c0
Roman <demkivroman5@gmail.com>
2019-12-24 22:09:12 +0200

    Add stream realization of class [Student]

---
+++ chapter_002/src/main/java/stream/Student.java
@@ -0,0 +1,13 @@
+package stream;
+
+public class Student {
+    private int score;
+
+    public Student(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
---
+++ chapter_002/src/test/java/stream/SchoolTest.java
@@ -0,0 +1,73 @@
+package stream;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SchoolTest {
+    @Test
+    public void whenCheckStudentsClassA() {
+        List<Student> students = List.of(
+                new Student(5),
+                new Student(90),
+                new Student(75),
+                new Student(1)
+        );
+        School school = new School();
+        List<Student> rsl = school.collect(students,
+                student -> student.getScore() >= 70
+                        && student.getScore() <= 100);
+
+        List<Integer> expect = List.of(90, 75);
+
+        assertThat(expect,
+                is(rsl.stream().map(
+                        score -> score.getScore()).
+                        collect(Collectors.toList())));
+    }
+    @Test
+    public void whenCheckStudentsClassB() {
+        List<Student> students = List.of(
+                new Student(50),
+                new Student(90),
+                new Student(69),
+                new Student(51)
+        );
+        School school = new School();
+        List<Student> rsl = school.collect(students,
+                student -> student.getScore() >= 50
+                        && student.getScore() <= 70);
+
+        List<Integer> expect = List.of(50, 69, 51);
+
+        assertThat(expect,
+                is(rsl.stream().map(
+                        score -> score.getScore()).
+                        collect(Collectors.toList())));
+    }
+    @Test
+    public void whenCheckStudentsClassC() {
+        List<Student> students = List.of(
+                new Student(50),
+                new Student(90),
+                new Student(69),
+                new Student(51)
+        );
+        School school = new School();
+        List<Student> rsl = school.collect(students,
+                student -> student.getScore() >= 0
+                        && student.getScore() <= 50);
+
+        List<Integer> expect = List.of(50);
+
+        assertThat(expect,
+                is(rsl.stream().map(
+                        score -> score.getScore()).
+                        collect(Collectors.toList())));
+    }
+}

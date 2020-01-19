999538e190daf39745e21115a36359677bde983c
Roman <demkivroman5@gmail.com>
2020-01-19 16:45:44 +0200

    Add realization Iterator<Iterator<Integer>>

---
+++ junior/src/main/java/Converter.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Converter {
+    public Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        List<Integer> list = new ArrayList<>();
+
+        it.forEachRemaining(
+                iter -> iter.forEachRemaining(
+                        el -> list.add(el)
+                )
+        );
+        return list.iterator();
+    }
+}

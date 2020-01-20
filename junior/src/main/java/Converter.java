d4770cc93dc6ccf092b71574fbb89206ec6c8968
Roman <demkivroman5@gmail.com>
2020-01-20 21:58:19 +0200

    Refactor Converter

--- junior/src/main/java/Converter.java
+++ junior/src/main/java/Converter.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class Converter {
     public Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
         List<Integer> list = new ArrayList<>();
-
         it.forEachRemaining(
                 iter -> iter.forEachRemaining(
                         el -> list.add(el)

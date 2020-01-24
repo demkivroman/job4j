2105cff29d99e80cc7577b5f7abc99a0e35e3f2c
Roman <demkivroman5@gmail.com>
2020-01-24 20:30:21 +0200

    refactor class [SimpleArray]

--- junior/src/main/java/generic/SimpleArray.java
+++ junior/src/main/java/generic/SimpleArray.java
@@ -20,13 +20,14 @@ public class SimpleArray<T> implements Iterable<T> {
         this.array[index++] = model;
     }
     public void set(int index, T model) {
-        Arrays.fill(this.array, index, index + 1, model);
+        this.array[index] = model;
     }
-    public void remove(int index) {
+    public boolean remove(int index) {
         this.array[index] = null;
         System.arraycopy(this.array, index + 1, this.array, index, this.array.length - index - 1);
         this.array[this.array.length - 1] = null;
         this.index--;
+        return true;
     }
     public T get(int index) {
         T rsl = (T) this.array[index];

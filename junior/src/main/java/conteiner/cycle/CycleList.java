3d018a728d3b6e7809ed34d3f8f63b4bbb4d225f
Roman <demkivroman5@gmail.com>
2020-02-17 20:18:59 +0200

    Refactor class [CycleList]

--- junior/src/main/java/conteiner/cycle/CycleList.java
+++ junior/src/main/java/conteiner/cycle/CycleList.java
@@ -3,23 +3,14 @@ package conteiner.cycle;
 public class CycleList {
     public static boolean hasCycle(Node first) {
         boolean rsl = false;
-        Node node = first;
-        Node nodeNext;
-        while (node.next != null) {
-            nodeNext = node.next;
-            nodeNext.previous = node;
-            Node prev = node.previous;
-            while (prev != null) {
-                if (prev == nodeNext) {
-                    rsl = true;
-                    break;
-                }
-                prev = prev.previous;
-            }
-            if (rsl) {
-                break;
-            }
-            node = nodeNext;
+        Node turtle = first.next;
+        Node hare = first.next != null ? first.next.next : null;
+        while (turtle != hare && hare != null) {
+            turtle = turtle.next;
+            hare = hare.next != null ? hare.next.next : null;
+        }
+        if (turtle == hare) {
+            rsl = true;
         }
         return rsl;
     }
--- junior/src/main/java/conteiner/cycle/Node.java
+++ junior/src/main/java/conteiner/cycle/Node.java
@@ -3,7 +3,6 @@ package conteiner.cycle;
 public class Node<T> {
     private T value;
     public Node next;
-    public Node previous;
 
     public Node(T value) {
         this.value = value;

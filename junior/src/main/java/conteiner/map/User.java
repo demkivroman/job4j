59e3db84d4be495bb5638e8fc1e802eac6b7d0c3
Roman <demkivroman5@gmail.com>
2020-02-19 18:12:59 +0200

    Refactor class [User] for <map> override 'equals' & 'hashcode'

--- junior/src/main/java/conteiner/map/User.java
+++ junior/src/main/java/conteiner/map/User.java
@@ -12,13 +12,13 @@ public class User {
         this.children = children;
         this.birthday = birthday;
     }
-    /*
+
     @Override
     public int hashCode() {
         int result = 31 * (this.name.hashCode() + this.children + this.birthday.hashCode());
         return result;
     }
- */
+
 
     @Override
     public boolean equals(Object obj) {

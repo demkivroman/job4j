e2386c187b007a6f5dda3ea8d15d15ace7818845
Roman <demkivroman5@gmail.com>
2020-03-01 17:41:05 +0200

    Overriding hashcode

--- junior/src/main/java/conteiner/map/User.java
+++ junior/src/main/java/conteiner/map/User.java
@@ -7,7 +7,9 @@ public class User {
     private int children;
     private Calendar birthday;
 
-    public User() {}
+    public User() {
+
+    }
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
@@ -17,7 +19,9 @@ public class User {
 
     @Override
     public int hashCode() {
-        int result = 31 * (this.name.hashCode() + this.children + this.birthday.hashCode());
+        int result = name.hashCode();
+        result = 31 * result + Integer.hashCode(children);
+        result = 31 * result + birthday.hashCode();
         return result;
     }
 
@@ -25,9 +29,9 @@ public class User {
     @Override
     public boolean equals(Object obj) {
         User userObj = (User) obj;
-        return this.name.equals(userObj.name) &&
-                this.children == userObj.children &&
-                this.birthday.equals(userObj.birthday);
+        return this.name.equals(userObj.name)
+                && this.children == userObj.children
+                && this.birthday.equals(userObj.birthday);
     }
 
     public static int hash(Object key) {

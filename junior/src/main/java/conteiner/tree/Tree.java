278d8a8e42780444e50b89d9fe6ce00254da0de7
Roman <demkivroman5@gmail.com>
2020-03-02 21:02:03 +0200

    Add class [Tree] and method <add>

---
+++ junior/src/main/java/conteiner/tree/Tree.java
@@ -0,0 +1,39 @@
+package conteiner.tree;
+
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
+
+public class Tree<E> implements SimpleTree<E> {
+    private final Node<E> root;
+    public Tree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    public boolean add(E parent, E child) {
+        boolean rsl = false;
+        Optional<Node<E>> parentEntry = findBy(parent);
+        Optional<Node<E>> childEntry = findBy(child);
+        if (parentEntry.isPresent() && childEntry.isEmpty()) {
+            Node<E> par = parentEntry.get();
+            par.children.add(new Node<>(child));
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+}

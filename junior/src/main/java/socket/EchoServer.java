0553bea0eab0e408f78e75cd2bfe1d0f432c5463
Roman <demkivroman5@gmail.com>
2020-03-25 18:33:47 +0200

    Realisation of Socket class [EchoServer]

---
+++ junior/src/main/java/socket/EchoServer.java
@@ -0,0 +1,33 @@
+package socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            boolean cycle = true;
+            String bye = "GET /?msg=Bye HTTP/1.1";
+            while (cycle) {
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(
+                             socket.getInputStream()))) {
+                    String str = in.readLine();
+                    if (str.equals(bye)) {
+                        cycle = false;
+                    }
+                    while (!str.isEmpty()) {
+                        System.out.println(str);
+                        str = in.readLine();
+                    }
+                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                }
+            }
+        }
+    }
+}

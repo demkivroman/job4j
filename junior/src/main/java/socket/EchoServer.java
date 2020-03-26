208ef910f7438b53408b20da577f1c5252fe5446
Roman <demkivroman5@gmail.com>
2020-03-26 16:41:32 +0200

    Refactor class [EchoServer] for Sockets

--- junior/src/main/java/socket/EchoServer.java
+++ junior/src/main/java/socket/EchoServer.java
@@ -8,24 +8,36 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+    public static String getParamater(String value) {
+        String rsl;
+        String[] arr = value.split("\\?");
+        String[] par = (arr.length > 1) ? arr[1].split(" ") : null;
+        rsl = (par != null) ? par[0].split("=")[1] : null;
+        return rsl;
+    }
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             boolean cycle = true;
-            String bye = "GET /?msg=Bye HTTP/1.1";
             while (cycle) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(new InputStreamReader(
                              socket.getInputStream()))) {
                     String str = in.readLine();
-                    if (str.equals(bye)) {
-                        cycle = false;
-                    }
+                    String par = getParamater(str) == null ? "" : getParamater(str);
                     while (!str.isEmpty()) {
                         System.out.println(str);
                         str = in.readLine();
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    String outStr;
+                     if (par.equals("Exit")) {
+                        cycle = false;
+                        outStr = "Terminate server work.";
+                    } else {
+                        outStr = par;
+                    }
+                    //out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    out.write(outStr.getBytes());
                 }
             }
         }
